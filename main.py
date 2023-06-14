0a17b4c25b9f2d533f171b8a5bf7a92e9a5ff737
dragonGR <alex@dragongr.dev>
2023-06-14 19:15:11 +0300

    PyHEIC2JPG: Refactor HEIC to JPG conversion and add progress update
    - Refactored the convert_heic_to_jpg function to improve code structure and readability.
    - Added a progress update feature to display the conversion progress as a percentage.
    - Updated the progress message to overwrite the previous progress on a single line.
    - Replaced the usage of logging.info with print for progress updates to avoid unexpected keyword argument errors.
    - Improved the overall efficiency and user experience of the HEIC to JPG conversion process.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
+import logging
 import pyheif
 from PIL import Image
 
+logging.basicConfig(level=logging.INFO, format='%(message)s')
 
 def convert_heic_to_jpg(heic_dir):
+    if not os.path.isdir(heic_dir):
+        logging.error(f"Directory '{heic_dir}' does not exist.")
+        return
+
     # Create a directory to store the converted JPG files
     jpg_dir = os.path.join(heic_dir, "ConvertedFiles")
     os.makedirs(jpg_dir, exist_ok=True)
@@ -12,32 +18,39 @@ def convert_heic_to_jpg(heic_dir):
     heic_files = [
         file for file in os.listdir(heic_dir) if file.lower().endswith(".heic")
     ]
+    total_files = len(heic_files)
 
     # Convert each HEIC file to JPG
-    for file_name in heic_files:
+    num_converted = 0
+    for file_index, file_name in enumerate(heic_files, start=1):
         heic_path = os.path.join(heic_dir, file_name)
         jpg_path = os.path.join(jpg_dir, os.path.splitext(file_name)[0] + ".jpg")
 
         try:
             # Open the HEIC file using pyheif
-            heif_file = pyheif.read(heic_path)
-            image = Image.frombytes(
-                heif_file.mode,
-                heif_file.size,
-                heif_file.data,
-                "raw",
-                heif_file.mode,
-                heif_file.stride,
-            )
+            with open(heic_path, "rb") as heic_file:
+                heif_file = pyheif.read(heic_file)
+                image = Image.frombytes(
+                    heif_file.mode,
+                    heif_file.size,
+                    heif_file.data,
+                    "raw",
+                    heif_file.mode,
+                    heif_file.stride,
+                )
 
             # Save the image as JPG
-            image.save(jpg_path, "JPEG", quality=100)
-            print(f"Converted: {file_name}")
+            with open(jpg_path, "wb") as jpg_file:
+                image.save(jpg_file, "JPEG", quality=100)
+                num_converted += 1
+
+            # Calculate and display the percentage progress
+            progress = int((file_index / total_files) * 100)
+            print(f"Conversion progress: {progress}%", end="\r", flush=True)
         except Exception as e:
-            print(f"Error converting {file_name}: {str(e)}")
+            logging.error(f"Error converting {file_name}: {str(e)}")
 
-    print("Conversion completed successfully.")
-
+    print("\nConversion completed successfully. {num_converted} files converted.")
 
 # Provide the directory path containing the HEIC files
 heic_directory = "/path/to/heic"
